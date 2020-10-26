ed89405b8504080b32db167ad36fe745109192ac
Lotus Lofgren <LotusLofgren@gmail.com>
2020-10-26 10:59:12 -0700

    Update remove_the_ANN_col.sh

--- remove_the_ANN_col.sh
+++ remove_the_ANN_col.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
 
+#when Jason changes the ANN col from ; sep to \t sep, just take it out before you process it. You don't really need it anyway. 
 n=$(head -n 1 Avian_CIFAR_5.snpEff.matrix.tsv | tr -dc \\t | tr \\t \\n | wc -l)
 cut -f1-$n Avian_CIFAR_5.snpEff.matrix.tsv > Avian_CIFAR_5.snpEff.matrix.fixed.tab
